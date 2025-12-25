f54a86a4ca646330bf6773d3673c82e5bac3321b
anas <a.mallouh522@gmail.com>
2025-12-25 21:51:55 +0200

    big finish frontend

--- client/src/Components/OpenedChats.jsx
+++ client/src/Components/OpenedChats.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom"
 export default function OpenedChats() {
   return (
     <div className='openedchats'>
-      <div style={{display:"flex" , justifyContent:"space-around"}} className="head">
+      <div style={{display:"flex" , justifyContent:"space-around" , margin:"10px 0"}} className="head">
         <h2 style={{display:"inline-block"}}>Chat</h2>
         <Link to={"/addchat"} style={{display:"inline-block" , fontSize:"24px" , textDecoration:"none" , color:"black"}}>+</Link>
       </div>
--- client/src/Pages/Logout.jsx
+++ client/src/Pages/Logout.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
-
+import { useNavigate } from "react-router-dom";
 export default function Logout() {
+
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    navigate('/')
+  } , [] )
   return (
     <div>
         <h3>Loging out ...</h3>
