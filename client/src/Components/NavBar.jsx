520094ad5a237c1368446e5e951ad99afef94afb
anas <a.mallouh522@gmail.com>
2026-01-28 17:32:04 +0200

    Start Servers futur

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -19,6 +19,10 @@ export default function NavBar({PageName , url1 , url2 , url3 , url4 , url5 , ur
         <nav>
             <h2>{PageName}</h2>
             <ul>
+              <li>
+                <input type="text" className="join_server" placeholder='Join Server'/>
+                <button className='Join'>🌐</button>
+              </li>
                 <li><Link to={url1[0]}>{url1[1]}</Link></li>
                 <li><Link to={url2[0]}>{url2[1]}</Link></li>
                 <li><Link to={url3[0]}>{url3[1]}</Link></li>
